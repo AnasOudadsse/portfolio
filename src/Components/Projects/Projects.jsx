c11d045288983f601f59cc74c3aa4550839236b6
Anas Oudadsse <anas.oudadsse1@gmail.com>
2024-09-08 22:07:46 +0100

    "Updated project descriptions, tags, and links in Projects component"

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -6,42 +6,42 @@ const projectsData = [
   {
     title: "Blood-Nation",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec urna ac tellus volutpat viverra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.",
+      "Blood-Nation est un système complet de gestion de banque de sang conçu pour rationaliser l'enregistrement des donneurs, les demandes des hôpitaux et le suivi des stocks de sang. L'application envoie des alertes en temps réel pour les pénuries critiques, garantissant une gestion efficace des ressources. Construit avec React, Tailwind CSS, Laravel et MySQL, le système a été optimisé à la fois pour l'expérience utilisateur et la fonctionnalité, offrant une navigation fluide et un backend robuste pour gérer les données et automatiser les processus.",
     imageSrc: "BloodNationHP.png", 
-    tags: ["React", "Tailwindcss", "Figma", "Laravel", "MySQL", "Git"],
+    tags: ["React", "Tailwindcss", "Laravel", "MySQL","Github", "Git", 'Chakra UI', "Composer", "Sanctum"],
     externalLink: "https://example.com", 
     repoLink: "https://github.com/AnasOudadsse/BloodBankManagement",
     flexDirection: ['column', 'column', 'column','row']
   },
   {
-    title: "My-coach",
+    title: "My-Coach Landing Page",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec urna ac tellus volutpat viverra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.",
+      "My-Coach est une plateforme conçue pour connecter les utilisateurs avec des coachs personnels. J'ai développé la page d'accueil en me concentrant sur un design épuré et une fonctionnalité optimale. Construite avec React et Chakra UI, la page est entièrement responsive et présente une mise en page intuitive. De plus, j'ai mis en place un système de soumission de formulaire avec Express.js en backend pour gérer les demandes et les requêtes des utilisateurs. Ce projet met en valeur ma capacité à intégrer des technologies front-end et back-end pour des interactions utilisateur fluides.",
     imageSrc: "My-coach-Lp.png", 
-    tags: ["React","Styled Components", "Tailwindcss", "Figma", "Laravel", "MySQL", "Git"],
+    tags: ["React","Tailwindcss", "Chakra UI", "Node.js",  "Express.js", "MongoDB", "Github", "Git"],
     externalLink: "https://my-coach-online.github.io/My-Coach-Landing-Page/", 
     repoLink: "https://github.com/AnasOudadsse/myCoach",
     flexDirection: ['column', 'column', 'column','row-reverse', ]
   },
   {
-    title: "X Capital UI/UX",
+    title: "X Capital (UI/UX)",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec urna ac tellus volutpat viverra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.",
+      "Un exemple de travail en UI/UX réalisé pour X Capital, axé sur la création de pages d'accueil réactives et visuellement attrayantes. Utilisant Figma, le projet met en évidence une attention particulière aux détails et un accent sur l'optimisation de l'expérience utilisateur tout en respectant les directives de la marque et les objectifs commerciaux.",
     imageSrc: "HomePage.jpg", 
-    tags: ["React","Styled Components", "Tailwindcss", "Figma", "Laravel", "MySQL", "Git"],
+    tags: ["Figma", "UI/UX Design"],
     externalLink: "https://www.figma.com/design/XIRuymUHVHqSp5IvtBRJpd/A-Sample-Of-My-Work-at-X-Capital?node-id=0-1&t=JHT48svd4058edeZ-1", 
     flexDirection: ['column', 'column', 'column','row'],
-    repoLink: null // No GitHub link, so GitHub icon should not display
+    repoLink: null 
   },
   {
-    title: "X Capital UI/UX",
+    title: "My-Coach Platform (UI/UX)",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec urna ac tellus volutpat viverra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.",
+      "Ce projet est une conception UI/UX pour la plateforme My-Coach, axée sur la page des détails des coachs et le processus de réservation. Le design met l'accent sur une navigation conviviale et une grande clarté, permettant aux utilisateurs de consulter facilement les profils des coachs, de lire les avis et de réserver des sessions. L'expérience utilisateur a été soigneusement conçue pour garantir un processus de réservation fluide avec un minimum d'étapes, en maintenant l'engagement des utilisateurs et en simplifiant les interactions avec les coachs potentiels.",
     imageSrc: "Coach details reviews.jpg", 
-    tags: ["React","Styled Components", "Tailwindcss", "Figma", "Laravel", "MySQL", "Git"],
+    tags: ["Figma", "UI/UX Design"],
     externalLink: "https://www.figma.com/design/rCQecy2Kg3UGGzOzATCqBw/My-Coach-Product-Demo?node-id=0-1&t=Rz05EXeqFoEYgdvX-1", 
     flexDirection: ['column', 'column', 'column','row-reverse'],
-    repoLink: null // No GitHub link
+    repoLink: null 
   },
 ];
 
@@ -85,7 +85,7 @@ export const Projects = () => {
             overflow="hidden"
             w="100%" 
             maxW="1300px" 
-            maxH={["900px","720px","600px","430px"]}
+            // maxH={["900px","720px","600px","430px"]}
           >
             {/* Project Image */}
             <Box  bg={imgBg} w={{ base: "100%", lg: "50%" }} h={["50%","50%","50%%",'100%']}>
