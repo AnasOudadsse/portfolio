d9fcb329e33efe26bf76e174eaa8219c273af477
Anas Oudadsse <anas.oudadsse1@gmail.com>
2024-09-17 17:32:02 +0100

    "Translated certification titles and descriptions from English to French, updated skills lists, and adjusted CSS styles for certification cards."

--- src/Components/Certif/Certif.jsx
+++ src/Components/Certif/Certif.jsx
@@ -17,40 +17,39 @@ import {
 
 const CertifData = [
     {
-        title: "Programming with JavaScript",
+        title: "Programmation avec JavaScript",
         date: "December 2023",
         issuer: "Meta",
-        description: "This certification focuses on core JavaScript programming concepts, including data structures, functions, objects, and asynchronous programming. It demonstrates a strong understanding of JavaScript's language features and best practices for building robust, scalable web applications.",
+        description: "Cette certification se concentre sur les concepts fondamentaux de la programmation en JavaScript, y compris les structures de données, les fonctions et les objets. Elle démontre une solide compréhension des fonctionnalités et des meilleures pratiques pour développer des applications web robustes et évolutives.",
         credetials : "https://coursera.org/verify/RB3VERHZ5NUN",
         skills : [
-            "JavaScript",
-            "Programming",
-            "Web Development"
+            "Objets",
+            "ES6+",
+            "Fonctions"
         ]
     },
     {
-        title: "React Basics",
+        title: "Les Bases de React",
         date: "December 2023",
         issuer: "Meta",
-        description: "This certification covers the fundamentals of React, including JSX, component structure, state management, and lifecycle methods. It provides a solid foundation in building interactive user interfaces using React and demonstrates the ability to manage complex UI logic and states efficiently.",
+        description: "Cette certification couvre tous les bases de React,  y compris JSX, la  structure des composants, la  gestion de l'état, et  les méthodes du cycle de vie. Elle  fournit une solide fondation pour la  création d'interfaces utilisateur  interactives avec React, démontre la capacité à gérer efficacement la logique d'interface et les états.",
         credetials : "https://coursera.org/verify/4SYCFES8XCL5",
         skills : [
-            "JavaScript",
-            "Programming",
-            "Web Development"
+            "JSX",
+            "Composants",
+            "Gestion d'état"
         ]
     },
     {
-        title: "Version Control",
+        title: "Contrôle de Version",
         date: "December 2023",
         issuer: "Meta",
-        description: "This certification provides expertise in version control systems, particularly Git and GitHub. It demonstrates the ability to track project changes, collaborate effectively with teams, and manage code repositories using industry-standard tools for version control.",
+        description: "Cette certification atteste de l'expertise dans les systèmes de contrôle de version, en particulier Git et GitHub. Elle démontre la capacité à suivre les modifications d'un projet, à collaborer efficacement avec des équipes, et à gérer des dépôts de code en utilisant des outils de contrôle de version reconnus dans l'industrie.",
         credetials : "https://coursera.org/verify/P58AGWZ4DRZ6",
         skills : [
-            "JavaScript",
-            "Programming",
-            "Web Development",
-            "OOP"
+            "Git",
+            "GitHub",
+            "Branches Git",
         ]
     },
 ]
@@ -90,7 +89,7 @@ export const Certif = () => {
             <Flex
             justify="center"
             align="center"
-            flexDirection={['column', 'column','column', 'row']}
+            flexDirection={['column', 'column','column','column', 'row']}
             >   
 
                 {
@@ -98,7 +97,8 @@ export const Certif = () => {
                         
                         <Box
                             key={index}
-                            maxW={["100%","100%","100%", "lg"]}
+                            maxW={["100%","lg","lg","lg", "lg"]}
+                            h={['750px','650px','650px','650px','700px']}
                             w='full'
                             bg={cardbg}
                             rounded="xl"
@@ -168,7 +168,7 @@ export const Certif = () => {
 
                             <Flex justify="center" align="center">
                                 <Button
-                                mt={5}
+                                mt={[10]}
                                 px={4}
                                 py={2}
                                 bg={buttonBg}
