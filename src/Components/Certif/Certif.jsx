c7adb5eda9d9546ed19df84f7d2e44e7c1b3e6b8
Anas Oudadsse <anas.oudadsse1@gmail.com>
2025-02-13 13:47:37 +0100

    Refactor Certif component for improved responsiveness and layout; added responsive values for card dimensions and adjusted content alignment.

--- src/Components/Certif/Certif.jsx
+++ src/Components/Certif/Certif.jsx
@@ -9,7 +9,8 @@ import {
     Icon,
     Tag,
     useColorModeValue,
-    VStack
+    VStack,
+    useBreakpointValue,
   } from "@chakra-ui/react";
   import { FiExternalLink } from "react-icons/fi";
   import { motion } from "framer-motion";
@@ -79,25 +80,28 @@ import {
       setFlippedCards(newFlippedState);
     };
   
+    // Responsive values
+    const cardWidth = useBreakpointValue({ base: "300px", sm: "350px", md: "400px", lg: "450px" });
+    const cardHeight = useBreakpointValue({ base: "200px", sm: "250px", md: "280px", lg: "300px" });
+    const headingSize = useBreakpointValue({ base: "md", md: "lg" });
+  
     return (
       <Box minH="100vh" bg={bg} py={21}>
         <VStack align="center" mt={14}>
-          <Heading size="lg" mb={4}>
+          <Heading size={headingSize} mb={4}>
             My Certifications
           </Heading>
         </VStack>
   
-        <Flex wrap="wrap" justify="center" align="center" gap={6} mt={10}>
+        <Flex wrap="wrap" justify="center" align="center" gap={6} mt={10} px={4}>
           {CertifData.map((certif, index) => (
-            <Box 
-              transform={'scale(0.8)'} 
-              key={index} 
-              width="450px"
-              height="300px"
-              style={{ 
+            <Box
+              key={index}
+              width={cardWidth}
+              height={cardHeight}
+              style={{
                 perspective: "1200px",
-            
-            }}
+              }}
               onMouseEnter={() => handleMouseEnter(index)}
               onMouseLeave={() => handleMouseLeave(index)}
             >
@@ -105,8 +109,8 @@ import {
                 position="relative"
                 width="full"
                 height="full"
-                style={{ 
-                    transformStyle: "preserve-3d" 
+                style={{
+                  transformStyle: "preserve-3d",
                 }}
                 animate={{ rotateY: flippedCards[index] ? 180 : 0 }}
                 transition={{ duration: 1 }}
@@ -114,8 +118,8 @@ import {
                 {/* Front Side - Certificate Image */}
                 <Box
                   position="absolute"
-                  width="450px"
-                  height="300px"
+                  width="full"
+                  height="full"
                   bg={cardbg}
                   shadow="lg"
                   rounded="xl"
@@ -140,69 +144,81 @@ import {
   
                 {/* Back Side - Certification Details */}
                 <Box
-                  position="absolute"
-                  width="450px"
-                  height="300px"
-                  bg={cardbg}
-                  shadow="lg"
-                  rounded="xl"
-                  display="flex"
-                  flexDirection="column"
-                  alignItems="center"
-                  justifyContent="center"
-                  textAlign="center"
-                  p={4}
-                  style={{
-                    backfaceVisibility: "hidden",
-                    transform: "rotateY(180deg)",
-                  }}
-                >
-                  <Heading as="h3" fontSize="lg" fontWeight="bold" color={color}>
-                    {certif.title}
-                  </Heading>
-                  <Text fontSize="sm" color="gray.500" mt={2}>
-                    Issuer: {certif.issuer}
-                  </Text>
-                  <Text fontSize="xs" color="gray.400" mt={1}>
-                    Date of Issue: {certif.date}
-                  </Text>
-                  <Text fontSize="md" color={color} mt={4} px={3}>
-                    {certif.description}
-                  </Text>
-  
-                  <Badge px={2} py={1} rounded="md" fontSize="xs" fontWeight="semibold" colorScheme="blue" mt={3}>
-                    Skills
-                  </Badge>
-  
-                  <Flex wrap="wrap" overflowWrap={2} mt={2} justify="center">
-                    {certif.skills && certif.skills.map((skill, skillIndex) => (
-                      <Tag key={skillIndex} bg={bg} color={color} m={1} px={2} py={1} rounded="md" fontSize="xs">
-                        {skill}
-                      </Tag>
-                    ))}
-                  </Flex>
-  
-                  <Button
-                    as="a"
-                    href={certif.credentials}
-                    target="_blank"
-                    mt={3}
-                    px={3}
-                    py={2}
-                    bg={buttonBg}
-                    color={buttonColor}
-                    fontWeight="semibold"
-                    rounded="lg"
-                    shadow="md"
-                    _hover={{ bg: buttonHoverBg }}
-                    _focus={{ outline: "none" }}
-                    transition="all 0.3s ease-in-out"
-                    fontSize="sm"
-                  >
-                    View Certificate
-                    <Icon ml={2} as={FiExternalLink} />
-                  </Button>
-                </Box>
+                    position="absolute"
+                    width="full"
+                    height="full"
+                    bg={cardbg}
+                    shadow="lg"
+                    rounded="xl"
+                    display="flex"
+                    flexDirection="column"
+                    alignItems="center"
+                    justifyContent="flex-start" // Changed to flex-start to align content from the top
+                    textAlign="center"
+                    p={{ base: 2, md: 4 }} // Reduced padding on mobile
+                    overflowY="auto" // Enable vertical scrolling if content overflows
+                    style={{
+                        backfaceVisibility: "hidden",
+                        transform: "rotateY(180deg)",
+                    }}
+                    >
+                    <Heading as="h3" fontSize={{ base: "sm", md: "lg" }} fontWeight="bold" color={color}>
+                        {certif.title}
+                    </Heading>
+                    <Text fontSize={{ base: "xs", md: "sm" }} color="gray.500" mt={1}>
+                        Issuer: {certif.issuer}
+                    </Text>
+                    <Text fontSize={{ base: "xs", md: "xs" }} color="gray.400" mt={1}>
+                        Date of Issue: {certif.date}
+                    </Text>
+                    <Text fontSize={{ base: "xs", md: "md" }} color={color} mt={2} px={2}>
+                        {certif.description}
+                    </Text>
+
+                    <Badge px={2} py={1} rounded="md" fontSize="xs" fontWeight="semibold" colorScheme="blue" mt={2}>
+                        Skills
+                    </Badge>
+
+                    <Flex wrap="wrap" overflowWrap={2} mt={1} justify="center">
+                        {certif.skills &&
+                        certif.skills.map((skill, skillIndex) => (
+                            <Tag
+                            key={skillIndex}
+                            bg={bg}
+                            color={color}
+                            m={0.5} // Reduced margin on mobile
+                            px={2}
+                            py={1}
+                            rounded="md"
+                            fontSize="xs"
+                            >
+                            {skill}
+                            </Tag>
+                        ))}
+                    </Flex>
+
+                    <Button
+                        size={'50'}
+                        as="a"
+                        href={certif.credentials}
+                        target="_blank"
+                        mt={5} // Reduced margin on mobile
+                        px={3}
+                        py={2} // Reduced padding on mobile
+                        bg={buttonBg}
+                        color={buttonColor}
+                        fontWeight="semibold"
+                        rounded="lg"
+                        shadow="md"
+                        _hover={{ bg: buttonHoverBg }}
+                        _focus={{ outline: "none" }}
+                        transition="all 0.3s ease-in-out"
+                        fontSize="sm"
+                    >
+                        View Certificate
+                        <Icon ml={2} as={FiExternalLink} />
+                    </Button>
+                    </Box>
               </MotionBox>
             </Box>
           ))}
@@ -210,4 +226,3 @@ import {
       </Box>
     );
   };
-  
