1adde0fe938dd6cc434a14ec76bad8885dab4736
Anas Oudadsse <anas.oudadsse1@gmail.com>
2025-02-13 12:04:27 +0100

    Added a blank line for improved code readability in App component

--- src/Components/Certif/Certif.jsx
+++ src/Components/Certif/Certif.jsx
@@ -9,205 +9,205 @@ import {
     Icon,
     Tag,
     useColorModeValue,
-    useColorMode,
     VStack
   } from "@chakra-ui/react";
   import { FiExternalLink } from "react-icons/fi";
-
-
-const CertifData = [
-    {
-        title: "Programmation avec JavaScript",
-        date: "December 2023",
-        issuer: "Meta",
-        description: "Cette certification se concentre sur les concepts fondamentaux de la programmation en JavaScript, y compris les structures de données, les fonctions et les objets. Elle démontre une solide compréhension des fonctionnalités et des meilleures pratiques pour développer des applications web robustes et évolutives.",
-        credetials : "https://coursera.org/verify/RB3VERHZ5NUN",
-        skills : [
-            "Objets",
-            "ES6+",
-            "Fonctions"
-        ]
-    },
-    {
-        title: "Les Bases de React",
-        date: "December 2023",
-        issuer: "Meta",
-        description: "Cette certification couvre tous les bases de React,  y compris JSX, la  structure des composants, la  gestion de l'état, et  les méthodes du cycle de vie. Elle  fournit une solide fondation pour la  création d'interfaces utilisateur  interactives avec React, démontre la capacité à gérer efficacement la logique d'interface et les états.",
-        credetials : "https://coursera.org/verify/4SYCFES8XCL5",
-        skills : [
-            "JSX",
-            "Composants",
-            "Gestion d'état"
-        ]
-    },
-    {
-        title: "Contrôle de Version",
-        date: "December 2023",
-        issuer: "Meta",
-        description: "Cette certification atteste de l'expertise dans les systèmes de contrôle de version, en particulier Git et GitHub. Elle démontre la capacité à suivre les modifications d'un projet, à collaborer efficacement avec des équipes, et à gérer des dépôts de code en utilisant des outils de contrôle de version reconnus dans l'industrie.",
-        credetials : "https://coursera.org/verify/P58AGWZ4DRZ6",
-        skills : [
-            "Git",
-            "GitHub",
-            "Branches Git",
-        ]
-    },
-    {
-        title: "Génie Logiciel",
-        date: "Novembre 2024",
-        issuer: "Alx Africa & Holberton School",
-        description: "Programme intensif axé sur le développement web et l’architecture logicielle, incluant front-end, back-end et gestion de projet. Acquisition de compétences en conception, déploiement et optimisation d’applications avec des technologies modernes.",
-        credetials : "https://coursera.org/verify/P58AGWZ4DRZ6",
-        skills : [
-            "Git",
-            "GitHub",
-            "Branches Git",
-        ]
-    },
-]
-
-export const Certif = () => {
-
-    const bg = useColorModeValue('gray.50', '#22272B');
-    const imgBg = useColorModeValue('#F5F6F7', '#282E33');
-    const color = useColorModeValue('black', 'white');
-    const cardbg = useColorModeValue('white', '#2C333A')  
-    const buttonColor = useColorModeValue('white', 'black');
-    const buttonBg = useColorModeValue('black', 'white');
-    const buttonHoverBg = useColorModeValue('gray.700', 'gray.300');
-
-    const colorMode = useColorMode()
+  import { motion } from "framer-motion";
+  import { useState } from "react";
   
-    console.log(colorMode);
-    
-
-
-  return (
-
-        <Box
-            minH="100vh"
-            bg={bg}
-            py={21}
-        >
-
-            <VStack align="center" mt={14}>
-                <Heading size="lg" mb={4}>
-                    Mes Certifications
-                </Heading>
-                <Text fontSize="lg" color={color} textAlign="center">
-                    Découvrez mes compétences validées à travers des certifications professionnelles
-                </Text>
-            </VStack>
-            <Flex
-            justify="center"
-            align="center"
-            flexDirection={['column', 'column','column','column', 'row']}
-            >   
-
-                {
-                    CertifData.map((certif, index) => (
-                        
-                        <Box
-                            key={index}
-                            maxW={["100%","lg","lg","lg", "lg"]}
-                            h={['750px','650px','650px','650px','700px']}
-                            w='full'
-                            bg={cardbg}
-                            rounded="xl"
-                            borderRadius={35}
-                            shadow="2xl"
-                            overflow="hidden"
-                            transform="scale(0.7)"
-                            transition="transform 0.5s"
-                            _hover={{ transform: "scale(0.72)" }}
-                        >
-                            <Flex 
-                                justify={'center'}                        
-                                bg={imgBg}
-                            >
-
-                            <Image
-                                w={300}
-                                h="40"
-                                objectFit="cover"
-                                src={colorMode.colorMode === "light" ? "Meta_lockup_positive primary_RGB.svg" : "Meta_lockup_mono_white_RGB.svg"} 
-                                alt="Nature scene"
-                                borderRadius={2000}
-                                p={5}
-                            />
-                        
-                            </Flex>
-                            <Box p={6}>
-                            <Heading as="h2" fontSize="2xl" fontWeight={600} mb={4} color={color}>
-                                {certif.title}
-                            </Heading>
-                            <Text fontSize={'lg'} color={color} mb={4}>
-                                {certif.description}
-                            </Text>
-                            <Badge
-                                my={5}
-                                px={2}
-                                py={1}
-                                rounded="md"
-                                fontSize="sm"
-                                fontWeight="semibold"
-                                colorScheme="blue"
-                            >
-                                Skills
-                            </Badge>
-                            <Flex wrap={'wrap'}>
-
-                                {
-                                    certif.skills.map((skill, index) => (
-                                        <Tag
-                                            key={index}
-                                            bg={imgBg}
-                                            color={color}
-                                            mr={2}
-                                            mb={2}
-                                            rounded="md"
-                                            px={2}
-                                            py={1}
-                                            fontSize="md"
-                                            fontWeight="semibold"
-                                            w={'fit-content'}
-                                        >
-                                            {skill}
-                                        </Tag>
-                                    ))
-                                }
-                                
-                            </Flex>
-
-                            <Flex justify="center" align="center">
-                                <Button
-                                as={'a'}
-                                href={certif.credetials}
-                                target="_blank"
-                                mt={[10]}
-                                px={4}
-                                py={2}
-                                bg={buttonBg}
-                                color={buttonColor}
-                                fontWeight="semibold"
-                                rounded="lg"
-                                shadow="md"
-                                _hover={{ bg: buttonHoverBg }}
-                                _focus={{ outline: "none", ring: 2, ringColor: "indigo.500", ringOpacity: 0.75 }}
-                                transition="all 0.3s ease-in-out"
-                                >
-                                credentials
-                                <Icon ml={2} boxSize="30px" borderRadius="md" p={1} as={FiExternalLink} color={buttonColor} />
-                                </Button>
-                            </Flex>
-
-                            </Box>
-                        </Box>
-                    )
-                )
-                }
-            </Flex>
-    </Box>
-        
-  );
-};
+  const MotionBox = motion(Box);
+  
+  const CertifData = [
+    {
+      title: "ALX Ventures Founder Academy",
+      date: "24th July 2024",
+      completionDate: "30th June 2024",
+      issuer: "ALX & Mastercard Foundation",
+      description: "For completing the ALX Ventures Founder Academy course and graduation requirements in 2024.",
+      credentials: "https://intranet.alxswe.com/certificates/2CyBRCJmep",
+      skills: ["Entrepreneurship", "Business Strategy", "Leadership"],
+      image: "alx-certif.png",
+    },
+    {
+      title: "Programming with JavaScript",
+      date: "22 December 2023",
+      issuer: "Meta & Coursera",
+      description: "An online non-credit course authorized by Meta and offered through Coursera.",
+      credentials: "https://coursera.org/verify/RB3VERHZ5NUN",
+      skills: ["JavaScript", "ES6+", "Functions"],
+      image: "js-certif.png",
+    },
+    {
+      title: "React Basics",
+      date: "24 December 2023",
+      issuer: "Meta & Coursera",
+      description: "An online non-credit course authorized by Meta and offered through Coursera.",
+      credentials: "https://coursera.org/verify/4SYCFES8XCL5",
+      skills: ["React", "JSX", "State Management"],
+      image: "React-certif.png",
+    },
+    {
+      title: "Version Control",
+      date: "25 December 2023",
+      issuer: "Meta & Coursera",
+      description: "An online non-credit course authorized by Meta and offered through Coursera.",
+      credentials: "https://coursera.org/verify/P58AGWZ4DRZ6",
+      skills: ["Git", "GitHub", "Version Control"],
+      image: "git-certif.png",
+    },
+  ];
+  
+  export const Certif = () => {
+    const bg = useColorModeValue("gray.50", "#22272B");
+    const color = useColorModeValue("black", "white");
+    const cardbg = useColorModeValue("white", "#2C333A");
+    const buttonColor = useColorModeValue("white", "black");
+    const buttonBg = useColorModeValue("black", "white");
+    const buttonHoverBg = useColorModeValue("gray.700", "gray.300");
+  
+    const [flippedCards, setFlippedCards] = useState(Array(CertifData.length).fill(false));
+  
+    const handleMouseEnter = (index) => {
+      const newFlippedState = [...flippedCards];
+      newFlippedState[index] = true;
+      setFlippedCards(newFlippedState);
+    };
+  
+    const handleMouseLeave = (index) => {
+      const newFlippedState = [...flippedCards];
+      newFlippedState[index] = false;
+      setFlippedCards(newFlippedState);
+    };
+  
+    return (
+      <Box minH="100vh" bg={bg} py={21}>
+        <VStack align="center" mt={14}>
+          <Heading size="lg" mb={4}>
+            My Certifications
+          </Heading>
+        </VStack>
+  
+        <Flex wrap="wrap" justify="center" align="center" gap={6} mt={10}>
+          {CertifData.map((certif, index) => (
+            <Box 
+              transform={'scale(0.8)'} 
+              key={index} 
+              width="450px"
+              height="300px"
+              style={{ 
+                perspective: "1200px",
+            
+            }}
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={() => handleMouseLeave(index)}
+            >
+              <MotionBox
+                position="relative"
+                width="full"
+                height="full"
+                style={{ 
+                    transformStyle: "preserve-3d" 
+                }}
+                animate={{ rotateY: flippedCards[index] ? 180 : 0 }}
+                transition={{ duration: 1 }}
+              >
+                {/* Front Side - Certificate Image */}
+                <Box
+                  position="absolute"
+                  width="450px"
+                  height="300px"
+                  bg={cardbg}
+                  shadow="lg"
+                  rounded="xl"
+                  border="2px solid gray"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  overflow="hidden"
+                  p={0.1}
+                  style={{ backfaceVisibility: "hidden" }}
+                >
+                  <Image
+                    src={certif.image}
+                    alt="Certificate"
+                    objectFit="cover"
+                    objectPosition="left"
+                    width="100%"
+                    height="100%"
+                    fallbackSrc="https://via.placeholder.com/400x250"
+                  />
+                </Box>
+  
+                {/* Back Side - Certification Details */}
+                <Box
+                  position="absolute"
+                  width="450px"
+                  height="300px"
+                  bg={cardbg}
+                  shadow="lg"
+                  rounded="xl"
+                  display="flex"
+                  flexDirection="column"
+                  alignItems="center"
+                  justifyContent="center"
+                  textAlign="center"
+                  p={4}
+                  style={{
+                    backfaceVisibility: "hidden",
+                    transform: "rotateY(180deg)",
+                  }}
+                >
+                  <Heading as="h3" fontSize="lg" fontWeight="bold" color={color}>
+                    {certif.title}
+                  </Heading>
+                  <Text fontSize="sm" color="gray.500" mt={2}>
+                    Issuer: {certif.issuer}
+                  </Text>
+                  <Text fontSize="xs" color="gray.400" mt={1}>
+                    Date of Issue: {certif.date}
+                  </Text>
+                  <Text fontSize="md" color={color} mt={4} px={3}>
+                    {certif.description}
+                  </Text>
+  
+                  <Badge px={2} py={1} rounded="md" fontSize="xs" fontWeight="semibold" colorScheme="blue" mt={3}>
+                    Skills
+                  </Badge>
+  
+                  <Flex wrap="wrap" overflowWrap={2} mt={2} justify="center">
+                    {certif.skills && certif.skills.map((skill, skillIndex) => (
+                      <Tag key={skillIndex} bg={bg} color={color} m={1} px={2} py={1} rounded="md" fontSize="xs">
+                        {skill}
+                      </Tag>
+                    ))}
+                  </Flex>
+  
+                  <Button
+                    as="a"
+                    href={certif.credentials}
+                    target="_blank"
+                    mt={3}
+                    px={3}
+                    py={2}
+                    bg={buttonBg}
+                    color={buttonColor}
+                    fontWeight="semibold"
+                    rounded="lg"
+                    shadow="md"
+                    _hover={{ bg: buttonHoverBg }}
+                    _focus={{ outline: "none" }}
+                    transition="all 0.3s ease-in-out"
+                    fontSize="sm"
+                  >
+                    View Certificate
+                    <Icon ml={2} as={FiExternalLink} />
+                  </Button>
+                </Box>
+              </MotionBox>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
+    );
+  };
+  
